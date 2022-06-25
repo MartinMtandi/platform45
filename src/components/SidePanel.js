a9f666d48e55f37efbfac78528c4a95e0d15c7d3
MartinMtandi <mtandimartin@gmail.com>
2022-06-25 21:17:13 +0200

    point arrow bottom for small screens

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -30,6 +30,26 @@ function SidePanel({setSvgState, svgState}) {
           point: 'right' 
         })
         break;
+      case 'bottom':
+        setSvgState({
+          ...svgState,
+          animate: {
+            rotate: 270,
+            transition: {duration: 1}
+          },
+          point: 'top' 
+        })
+        break;
+      case 'top':
+        setSvgState({
+          ...svgState,
+          animate: {
+            rotate: 90,
+            transition: {duration: 1}
+          },
+          point: 'bottom' 
+        })
+        break;
       default:
         break;
     }
@@ -44,12 +64,12 @@ function SidePanel({setSvgState, svgState}) {
         <h2 className="font-bold md:text-4xl text-3xl text-charcoal-grey-900">Front-end challenge!</h2>
         <p className="text-charcoal-grey-900 text-xl md:text-2xl mt-10 mb-16">This is a design that you will need to <br /> code up and impress us.</p>
         <button onClick={handleAnimate} className="focus:outline-none bg-charcoal-grey-900 rounded-full inline-flex items-center justify-center w-16 h-16">
-          <motion.svg className={`h-8 w-8 ${(svgState.point === 'left') ? 'ml-5 mt-1' : 'ml-0 mt-0'} `} xmlns="http://www.w3.org/2000/svg" width="17" height="29" viewBox="0 0 17 29"
+          <motion.svg className={`h-8 w-8 ${(svgState.point === 'left') ? 'ml-5 mt-1' : (svgState.point === 'bottom') ? 'ml-4 mb-1' : (svgState.point === 'top') ? 'ml-2 mt-3' : 'ml-0 mt-0'} `} xmlns="http://www.w3.org/2000/svg" width="17" height="29" viewBox="0 0 17 29"
             variants={svgVariants}
             initial="initial"
             animate="animate"
           >
-            <path fill="#FFD300" fill-rule="evenodd" d="M16.969 14.502c0-.519-.195-1.038-.585-1.433L4.128.619a1.974 1.974 0 0 0-2.823 0 2.05 2.05 0 0 0 0 2.867L12.15 14.502 1.306 25.518a2.05 2.05 0 0 0 0 2.867c.78.793 2.043.793 2.823 0l12.256-12.45a2.04 2.04 0 0 0 .584-1.433" />
+            <path fill="#FFD300" fillRule="evenodd" d="M16.969 14.502c0-.519-.195-1.038-.585-1.433L4.128.619a1.974 1.974 0 0 0-2.823 0 2.05 2.05 0 0 0 0 2.867L12.15 14.502 1.306 25.518a2.05 2.05 0 0 0 0 2.867c.78.793 2.043.793 2.823 0l12.256-12.45a2.04 2.04 0 0 0 .584-1.433" />
           </motion.svg>
         </button>
       </div>
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -4,23 +4,67 @@ import MyWorld from '../../components/MyWorld';
 import SidePanel from '../../components/SidePanel';
 
 function Register() {
+
   const [svgState, setSvgState] = React.useState({
-    initial: { rotate: 0},
+    initial: { rotate: 0 },
     animate: {
       rotate: 0,
-      transition: {duration: 0}
+      transition: { duration: 0 }
     },
-    point: 'right' 
+    point: 'right'
   });
+
+  const [screenSize, getDimension] = React.useState({
+    dynamicWidth: window.innerWidth,
+  });
+
+  const setDimension = () => {
+    getDimension({
+      dynamicWidth: window.innerWidth,
+    })
+  }
+
+  console.log(screenSize.dynamicWidth);
+
+  React.useEffect(() => {
+    if(screenSize.dynamicWidth <= 420){
+      setSvgState({
+        ...svgState,
+        animate: {
+          rotate: 90,
+          transition: { duration: 1 }
+        },
+        point: 'bottom'
+      })
+    }else{
+      setSvgState({
+        ...svgState,
+        initial: { rotate: 0 },
+        animate: {
+          rotate: 0,
+          transition: { duration: 0 }
+        },
+        point: 'right'
+      })
+    }
+  }, [screenSize.dynamicWidth]);
+
+  React.useEffect(() => {
+    window.addEventListener('resize', setDimension);
+
+    return (() => {
+      window.removeEventListener('resize', setDimension);
+    })
+  }, [screenSize.dynamicWidth])
 
   return (
     <div className="md:grid md:grid-cols-5 gap-0 md:h-screen">
-        <div className="md:col-span-2">
-          <SidePanel svgState={svgState} setSvgState={setSvgState} />
-        </div>
-        <div className="md:col-span-3">
-          {(svgState.point === 'right') ? <MainForm /> : <MyWorld />}
-        </div>
+      <div className="md:col-span-2">
+        <SidePanel svgState={svgState} setSvgState={setSvgState} />
+      </div>
+      <div className="md:col-span-3">
+        {(svgState.point === 'right') ? <MainForm /> : <MyWorld />}
+      </div>
     </div>
   )
 }
